645024b9ddd8c17bdb6225380db646ab8c3c51e3
Brandon OddDuckk <Brandon.Ogaden@gmail.com>
2024-06-14 21:45:11 +0800

    Added history chart

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ local_tz = pytz.timezone(timezone)
 # Initialize counters
 time_init = datetime.now(local_tz)
 counters = {
-    'fresh': 0,
-    'rotten': 0,
-    'last_reset': time_init.date() # time_init.date() - timedelta(days=1)
+    'fresh': 50,
+    'rotten': 25,
+    'last_reset': time_init.date() # - timedelta(days=1)
 }
 
 # Lock for thread-safe counter updates
@@ -42,10 +42,10 @@ counter_lock = threading.Lock()
 
 def reset_counters():
     global counters
-    with app.app_context():  # Ensure this function runs within the Flask app context
+    with app.app_context():
         while True:
             now = datetime.now(local_tz)
-            logging.info(f"Current time: {now}")
+            logging.info(f"Current date: {now.date()}")
             logging.info(f"Last reset: {counters['last_reset']}")
             
             if now.date() != counters['last_reset']:
@@ -68,9 +68,11 @@ def reset_counters():
                     except Exception as e:
                         db.session.rollback()
                         logging.error(f"Failed to reset counters: {e}")
+            else:
+                logging.info("No reset needed.")
             
             # Sleep for a short period to reduce CPU usage
-            time.sleep(60)
+            time.sleep(5)
 
 @app.route('/')
 def index():
@@ -93,6 +95,22 @@ def update_counter():
 def get_count():
     global counters
     return jsonify(counters)
+
+@app.route('/history', methods=['GET'])
+def get_history():
+    days = request.args.get('days', default=7, type=int)
+    end_date = datetime.now(local_tz).date()
+    start_date = end_date - timedelta(days=days)
+    
+    records = TomatoCount.query.filter(TomatoCount.date.between(start_date, end_date)).all()
+    
+    history = {
+        'dates': [record.date.strftime('%Y-%m-%d') for record in records],
+        'fresh_counts': [record.fresh_count for record in records],
+        'rotten_counts': [record.rotten_count for record in records]
+    }
+    
+    return jsonify(history)
 
 if __name__ == '__main__':
     # Ensure the database and table are created
